fe12cf244378b65d21d7f846cf9f27a948a669c8
joshkenney <joshuagkenney@gmail.com>
2021-01-17 19:57:52 -0500

    refactored

--- exp/timeline.js
+++ exp/timeline.js
@@ -397,20 +397,5 @@ var feedback_pleasant = {
     choices: jsPsych.NO_KEYS,
   };
 
-
-let instructions = {
-    timeline: [instructions_1, instructions_2, instructions_3, instructions_4, instructions_5, instructions_6, instructions_7, instructions_8, instructions_9, instructions_10],
-  }
-
-var procedure = {
-    timeline: [fixation, /*prompt_pleasant,*/ response_pleasant, feedback_pleasant, /*prompt_unpleasant,*/ response_unpleasant, feedback_unpleasant, /*prompt_arousal,*/ response_arousal, feedback_arousal],
-    timeline_variables: full_stim_shuffle,
-    //randomize_order: false
-  }
-
-
-timeline.push(welcome);
-timeline.push(instructions);
-timeline.push(procedure);
-timeline.push(save_data);
-timeline.push(end);
+// call main
+$.getScript("exp/main.js");
